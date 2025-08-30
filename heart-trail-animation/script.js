d9e88e161e597b286a877c3311ceee458ab2f62c
Mohit Kumar <mohitkumarudn802210@gmail.com>
2025-08-30 20:30:36 +0530

    Add files via upload

---
+++ heart-trail-animation/script.js
@@ -0,0 +1,18 @@
+const bodyEl = document.querySelector('body');
+
+bodyEl.addEventListener('mousemove', (e) => {
+  const xMove = e.offsetX;
+  const yMove = e.offsetY;
+
+  const spanEl = document.createElement('span');
+  spanEl.style.left = xMove + 'px';
+  spanEl.style.top = yMove + 'px';
+  const size = Math.random() * 100;
+  spanEl.style.width = size + 'px';
+  spanEl.style.height = size + 'px';
+  bodyEl.appendChild(spanEl);
+
+  setTimeout(() => {
+    spanEl.remove();
+  }, 3000);
+});
